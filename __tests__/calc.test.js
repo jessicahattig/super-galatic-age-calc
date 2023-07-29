5efc922a7fbc9b7c0099d5f895f3b851d4e2a5b7
jessicahattig <jessicahattig@gmail.com>
2023-07-29 14:44:28 -0700

    Function 1, test 2. Red phase.

--- __tests__/calc.test.js
+++ __tests__/calc.test.js
@@ -7,4 +7,10 @@ test('Calculates age in Mercury years', () => {
   const mercuryAge = ageCalc.calculateMercuryAge();
   expect(mercuryAge).toBeCloseTo(124.60, 1);
   });
+
+test('Calculates age in Venus years', () => {
+  const ageCalc = new Calc(30);
+  const venusAge = ageCalc.calculateVenusAge();
+  expect(venusAge).toBeCloseTo(48.75, 4);
+  });
 });
