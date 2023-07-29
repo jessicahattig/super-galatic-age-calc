637eaa2e0a71a158d5591c48bc862ebcf13f8b88
jessicahattig <jessicahattig@gmail.com>
2023-07-29 13:59:32 -0700

    Function 1, test 1. Red phase.

--- __tests__/calc.test.js
+++ __tests__/calc.test.js
@@ -1,5 +1,10 @@
 import { Calc } from '../src/js/calc.js'; 
 
-describe("Super Galactic Age Calculator", () => {
-  
-}
+describe("Super Galactic Age Calculator Tests", () => {
+
+test('Calculates age in Mercery years', () => {
+  const ageCalc = new GalaticAgeCalc(30);
+  const mercuryAge = ageCalc.GalaticAgeCalc();
+  expect(mercuryAge).toBeClostTo(125);
+  });
+});
